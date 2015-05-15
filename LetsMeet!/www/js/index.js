f6ca3afde2b0f878d05a3f4c898b5d5c9c9b5f5d
Danny Coenen <info@dannycoenen.nl>
2015-05-15 12:25:20 +0200

    Opslag lokale database
    Het aanmaken en opslaan van gegevens in een lokale database

--- LetsMeet!/www/js/index.js
+++ LetsMeet!/www/js/index.js
@@ -1,49 +1,50 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// Wait for Cordova to load
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+	// Populate the database 
+	function populateDB(tx) {
+		tx.executeSql('DROP TABLE IF EXISTS USERDATA');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS USERDATA (id unique, phone, name)');
+		tx.executeSql('INSERT INTO USERDATA (id, phone, name) VALUES (1, "0615556393", "Danny Coenen")');
+	}
+	
+	// Query the database
+	function queryDB(tx) {
+		tx.executeSql('SELECT * FROM USERDATA', [], querySuccess, errorCB);
+	}
+	
+	// Query the success callback
+	function querySuccess(tx, results) {
+		console.log("Returned rows = " + results.rows.length);
+		
+		var getName = document.getElementById('myName');
+		var resultName= results.rows.item(0).name;
+		getName.innerHTML = resultName;
+		
+		var getPhone = document.getElementById('myPhone');
+		var resultPhone= results.rows.item(0).phone;
+		getPhone.innerHTML = resultPhone;
+		
+		// this will be true since it was a select statement and so rowsAffected was 0
+		if (!results.rowsAffected) {
+			console.log('No rows affected!');
+			return false;
+		}
+	}
+	
+	// Transaction error callback
+		function errorCB(err) {
+		console.log("Error processing SQL: "+err.code);
+	}
+	
+	// Transaction success callback
+	function successCB() {
+		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+		db.transaction(queryDB, errorCB);
+	}
+	
+	// Cordova is ready
+	function onDeviceReady() {
+		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+		db.transaction(populateDB, errorCB, successCB);
+	}
