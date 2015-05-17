111be8bbcf0aaaf232db7eb2afcbd894fb63cc4b
tl1992 <info@morgenmedia.nl>
2015-05-17 17:23:47 +0200

    Location toegevoegd en haalt resultaten op
    Resultaten ophalen uit de Places Libarary van Google en die op een map
    tonen.

--- LetsMeet!/www/js/maps.js
+++ LetsMeet!/www/js/maps.js
@@ -1,89 +1,80 @@
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+var map, placesList;
 
-function onSuccess(position) {
-   var current_lat = position.coords.latitude;
-   var current_lng = position.coords.longitude;
-   var secheltLoc = new google.maps.LatLng(current_lat, current_lng);
+function initialize() {
 
- var myMapOptions = {
-   zoom: 16,
-   center: secheltLoc,
-   mapTypeId: google.maps.MapTypeId.HYBRID
- };
- var theMap = new google.maps.Map(document.getElementById("map_canvas"), myMapOptions);
- var image = "img/map_pin.png"
- var marker = new google.maps.Marker({
- map: theMap,
- draggable: false,
- position: new google.maps.LatLng(current_lat, current_lng),
- visible: true,
- icon: image,
- title:'Title' // Title
-});
-
- var myOptions = {
-	  content: "",
-	  disableAutoPan: false,
-	  maxWidth: 0,
-	  pixelOffset: new google.maps.Size(-140, -110),
-	  pixelOffset: new google.maps.Size(140, 110),
-	  zIndex: null,
-	  boxStyle: { 
-		  background: "url('tipbox.gif') no-repeat",
-		  opacity: 0.90
-	 },
-	 closeBoxMargin: "10px 2px 2px 2px",
-	 closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
-	 infoBoxClearance: new google.maps.Size(1, 1),
-	 isHidden: false,
-	 pane: "floatPane",
-	 enableEventPropagation: false
- };
-
- var contentString = '<div class="map_anotaion_title">Yor Content</div>'; //Address on pin click
-
-var infowindow = new google.maps.InfoWindow({
-  content: contentString
- });
- infowindow.open(theMap,marker); 
- google.maps.event.addListener(marker, "click", function (e) {
-    infowindow.open(theMap,marker);     
- });
-}
-
-function onError(error)
-{
-   alert(error) ;   
-}
-
-/*
-$(document).ready(function(){
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-function onSuccess(position) {
-   var current_lat = position.coords.latitude;
-   var current_lng = position.coords.longitude;
-		$('.output').append('<li>Loading Sam Croft\'s twkjhhjjhghgeets');	
-
-		$.jsonp({
-			url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+current_lat+','+current_lng+'&radius=1000&types=food&key=AIzaSyAbEK1wNjuaWBJ2PsGkKslY_zFR6SjaFCo',
-			callbackParameter: 'callback',
-			success: function(data, status) {
-				//$('#your-tweets').append('<li>The feed loads fine');
-				$.each(data, function(i,item){ 
-					var tweet = item.results[i].name;
-					$('.output').append('<li>'+tweet);
-				});
-			},
-			error: function(){
-				$('output').append('<li>There was an error loading the feed');
-			}
-		});		
+	navigator.geolocation.getCurrentPosition(onSuccess);
 	
 	
-}*/
-function onError(error)
-{
-   alert(error) ;   
+	function onSuccess(position) {
+		var current_lat = position.coords.latitude;
+		var current_lng = position.coords.longitude;
+		var pyrmont = new google.maps.LatLng(current_lat, current_lng);
+		
+		 map = new google.maps.Map(document.getElementById('map-canvas'), {
+			center: pyrmont,
+			zoom: 17
+		 });
+		 
+		 var request = {
+			location: pyrmont,
+			radius: 500,
+			types: ['store']
+		  };
+
+		  placesList = document.getElementById('places');
+
+		  var service = new google.maps.places.PlacesService(map);
+		  service.nearbySearch(request, callback);
+			
+	}
+
+	  
 }
-//});
+
+function callback(results, status, pagination) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    return;
+  } else {
+    createMarkers(results);
+
+    if (pagination.hasNextPage) {
+      var moreButton = document.getElementById('more');
+
+      moreButton.disabled = false;
+
+      google.maps.event.addDomListenerOnce(moreButton, 'click',
+          function() {
+        moreButton.disabled = true;
+        pagination.nextPage();
+      });
+    }
+  }
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+
+    placesList.innerHTML += '<li>' + place.name + '</li>';
+
+    bounds.extend(place.geometry.location);
+  }
+  map.fitBounds(bounds);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
