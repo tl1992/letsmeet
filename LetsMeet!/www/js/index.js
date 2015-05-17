dd9fe432c71a472a49b72afe5df640fbd1f55dad
Danny Coenen <info@dannycoenen.nl>
2015-05-17 15:10:47 +0200

    Locale opslag/database aanpassing
    De locale database was te ingewikkeld en langzaam. Daarom is deze
    vervangen door HTML 5 local storage.

--- LetsMeet!/www/js/index.js
+++ LetsMeet!/www/js/index.js
@@ -1,42 +1,12 @@
-// Wait for Cordova to load
-document.addEventListener("deviceready", onDeviceReady, false);
-
-	// Populate the database 
-	function populateDB(tx) {
-		tx.executeSql('DROP TABLE IF EXISTS USERDATA');
-		tx.executeSql('CREATE TABLE IF NOT EXISTS USERDATA (id unique, phone, name)');
-		tx.executeSql('INSERT INTO USERDATA (id, phone, name) VALUES (1, "0615556393", "Danny Coenen")');
+function inviteForm() {
+	var name = document.forms["invitation"]["fname"].value;
+	var phone = document.forms["invitation"]["fphone"].value;
+	if (name == null || phone == "") {
+		alert("Vul een naam en telefoon nummer in");
+		return false;
 	}
-	
-	// Query the database
-	function queryDB(tx) {
-		tx.executeSql('SELECT * FROM USERDATA', [], querySuccess, errorCB);
-	}
-	
-	function querySuccess(tx, results) {
-  		console.log("Returned rows = " + results.rows.length);
-  		// this will be true since it was a select statement and so rowsAffected was 0
-	  	if (!results.rowsAffected) {
-			console.log('No rows affected!');
-			return false;
-	  	}
-  		// for an insert statement, this property will return the ID of the last inserted row
-  		console.log("Last inserted row ID = " + results.insertId);
-    }
-	
-	// Transaction error callback
-		function errorCB(err) {
-		console.log("Error processing SQL: "+err.code);
-	}
-	
-	// Transaction success callback
-	function successCB() {
-		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-		db.transaction(queryDB, errorCB);
-	}
-	
-	// Cordova is ready
-	function onDeviceReady() {
-		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-		db.transaction(populateDB, errorCB, successCB);
-	}
+	localStorage.setItem("name", name);
+	localStorage.setItem("phone", phone);
+	window.location = "dashboard.html";
+	return false;
+}
