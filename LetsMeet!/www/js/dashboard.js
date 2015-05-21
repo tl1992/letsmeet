1671d18ea986bdc9e6838aaedca306056a413766
Danny Coenen <info@dannycoenen.nl>
2015-05-21 17:30:37 +0200

    Login bug verholpen en uitnodigingen aanpassingen
    Het login scherm wordt nu alleen getoond wanneer er nog geen gebruikers
    gegevens bestaan.
    
    Voor nieuwe uitnodigingen wordt er nu een bolletje getoond met een
    cijfer voor het aantal nieuwe uitnodigingen.
    
    Meldingen voor uitnodigingen worden nu getoond met naam.

--- LetsMeet!/www/js/dashboard.js
+++ LetsMeet!/www/js/dashboard.js
@@ -1,7 +1,41 @@
+//localStorage.clear();
+
 var myName = localStorage.getItem("name");
 var myPhone = localStorage.getItem("phone");
 document.getElementById("myName").innerHTML = myName;
 document.getElementById("myPhone").innerHTML = myPhone;
+
+
+setInterval(function(){
+	$.ajax({
+	type: "GET",
+	url: "http://dannycoenen.nl/app/letsmeet/check.php?name=" + myName + "&phone=" + myPhone,
+	dataType: "json",
+	success: function(data) {
+		$('#count').remove();
+		var notifications = data.length;
+		$('<div id="count"></div>').insertAfter('#notification');
+		document.getElementById("count").innerHTML = notifications;
+	},
+	error: function(data) {
+		//alert("ERROR");
+	}
+	});
+},5000);
+
+	$.ajax({
+	type: "GET",
+	url: "http://dannycoenen.nl/app/letsmeet/check.php?name=" + myName + "&phone=" + myPhone,
+	dataType: "json",
+	success: function(data) {
+		var notifications = data.length;
+		$('<div id="count"></div>').insertAfter('#notification');
+		document.getElementById("count").innerHTML = notifications;
+	},
+	error: function(data) {
+		//alert("ERROR");
+	}
+	});
 
 function checkForInvites() {
 	var myName = localStorage.getItem("name");
@@ -12,10 +46,13 @@ function checkForInvites() {
 		url: "http://dannycoenen.nl/app/letsmeet/check.php?name=" + myName + "&phone=" + myPhone,
 		dataType: "json",
 		success: function(data) {
-			alert(data);
+			for (var i = 0; i < data.length;) {
+				alert('Je hebt een nieuwe uitnodiging van: ' + data[i].firstUser);
+				i++;
+			}
 		},
 		error: function(data) {
-			alert("ERROR");
+			alert("Geen nieuwe meldingen");
 		}
 	});
 }
