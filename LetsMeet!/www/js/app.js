ba63483fce9273cdd57b33cd0e042c6b680b5027
tl1992 <info@morgenmedia.nl>
2015-05-29 20:49:55 +0200

    Geolocation
    Geolocation geoptimaliseerd zodat deze werkt in angularjs

--- LetsMeet!/www/js/app.js
+++ LetsMeet!/www/js/app.js
@@ -75,6 +75,7 @@
 	}
 
   });
+   
   
   module.controller('InviteContactController', function($scope) {
 	$scope.inviteContact = function() {
@@ -104,8 +105,69 @@
 	}
   });
   
+  var app = angular.module("geo", ["ui.map", "ui.event"])
+    module.controller("MapController", function ($scope) {
+        $scope.lat = "0";
+        $scope.lng = "0";
+        $scope.accuracy = "0";
+        $scope.error = "";
+        $scope.model = { myMap: undefined };
+        $scope.myMarkers = [];
+
+        $scope.showResult = function () {
+            return $scope.error == "";
+        }
+
+        $scope.mapOptions = {
+            center: new google.maps.LatLng($scope.lat, $scope.lng),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        $scope.showPosition = function (position) {
+            $scope.lat = position.coords.latitude;
+            $scope.lng = position.coords.longitude;
+            $scope.accuracy = position.coords.accuracy;
+            $scope.$apply();
+
+            var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
+			alert($scope.model.myMap);
+            $scope.model.myMap.setCenter(latlng);
+            $scope.myMarkers.push(new google.maps.Marker({ map: $scope.model.myMap, position: latlng }));
+        }
+
+        $scope.showError = function (error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    $scope.error = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    $scope.error = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    $scope.error = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    $scope.error = "An unknown error occurred."
+                    break;
+            }
+            $scope.$apply();
+        }
+
+        $scope.getLocation = function () {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+            }
+            else {
+                $scope.error = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        $scope.getLocation();
+    });
   
-  module.controller('MapController', function ($scope) {
+  
+  /*module.controller('MapController', function ($scope) {
 		  
 		var map, placesList;
 
@@ -217,7 +279,7 @@
 
 		//google.maps.event.addDomListener(window, 'load', initialize);
   
-  });
+  });*/
  
   
 })();
