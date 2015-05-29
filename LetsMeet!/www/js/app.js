11dec23c999bf2d5635d21a05c820de15eb2a71b
tl1992 <info@morgenmedia.nl>
2015-05-29 17:05:57 +0200

    Oplossen conflict

--- LetsMeet!/www/js/app.js
+++ LetsMeet!/www/js/app.js
@@ -1,15 +1,20 @@
 (function(){
   'use strict';
   var module = angular.module('app', ['onsen']);
-
-  module.controller('AppController', function($scope, $data) {
-    $scope.doSomething = function() {
-      alert('Hello, World!');
-    };
-	
+  
+  module.factory('$data', function() {
+      var data = {};
+      
+      data.items = [
+      ]; 
+      
+      return data;
   });
   
-  module.controller('InviteController', function($scope, $data) {
+// ####################################################################
+// Registration page
+// ####################################################################
+  module.controller('LoginController', function($scope, $data) {
 	$scope.register = function() {
 		var name = $scope.name;
 		var phone = $scope.phone;
@@ -23,35 +28,178 @@
 		return false;
 	}
   });
-
-  module.factory('$data', function() {
-      var data = {};
-      
-      data.items = [
-          { 
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          { 
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          }
-      ]; 
-      
-      return data;
+  
+// ####################################################################
+// Dashboard page
+// ####################################################################
+  module.controller('NotificationController', function($scope, $data) {
+	$scope.check = function() {
+		var myName = localStorage.getItem("name");
+		var myPhone = localStorage.getItem("phone");
+		
+		$.ajax({
+			type: "GET",
+			url: "http://dannycoenen.nl/app/letsmeet/check.php?name=" + myName + "&phone=" + myPhone,
+			dataType: "json",
+			success: function(data) {
+				for (var i = 0; i < data.length;) {
+					alert('Je hebt een nieuwe uitnodiging van: ' + data[i].firstUser);
+					i++;
+				}
+			},
+			error: function(data) {
+				alert("Geen nieuwe meldingen");
+			}
+		});
+	}
   });
+  
+  module.controller('InviteController', function($scope, $data) {
+	$scope.invite = function() {
+		
+		var myName = localStorage.getItem("name");
+		var myPhone = localStorage.getItem("phone");
+	
+		var secName = $scope.name;
+		var secPhone = $scope.phone;
+		
+		if (secName == null || secPhone == "") {
+			alert("Vul een naam en telefoon nummer in");
+			return false;
+		}
+		
+		$.ajax({
+			type: "GET",
+			url: "http://dannycoenen.nl/app/letsmeet/register.php?name=" + myName + "&phone=" + myPhone + "&secName=" + secName + "&secPhone=" + secPhone,
+			dataType: "json",
+			success: function(data) {
+				alert(data);
+			},
+			error: function(data) {
+				alert("ERROR");
+			}
+		});
+		
+		return false;
+	}
+  });
+  
+  
+  
+  module.controller('MapController', function ($scope) {
+		  
+		var map, placesList;
+
+		var placeSearch, autocomplete;
+
+		$scope.initialize = function () {
+			//alert(223456);
+			
+			
+			$scope.getLocation = function () {
+				navigator.geolocation.getCurrentPosition($scope.onSuccess);
+			}
+	 
+			$scope.getLocation();
+			
+			//navigator.geolocation.getCurrentPosition($scope.onSuccess);
+			
+			$scope.onSuccess = function (position) {
+				alert(456);
+				var input = document.getElementById('searchTextField');
+				var autocomplete = new google.maps.places.Autocomplete(input);
+				google.maps.event.addListener(autocomplete, 'place_changed', function () {
+					var place = autocomplete.getPlace();
+					$scope.city2 = place.name;
+					$scope.cityLat = place.geometry.location.lat();
+					$scope.cityLng = place.geometry.location.lng();
+					//alert("This function is working!");
+					//alert(place.name);
+				   // alert(place.address_components[0].long_name);
+				   
+					$scope.current_lat = position.coords.latitude;
+					$scope.current_lng = position.coords.longitude;	
+
+					var averageLat = (position.coords.latitude+place.geometry.location.lat())/2;
+					var averageLng = (position.coords.longitude+place.geometry.location.lng())/2;  
+					
+					var pyrmont = new google.maps.LatLng(averageLat, averageLng);
+
+					
+					 $scope.map = new google.maps.Map(document.getElementById('map-canvas'), {
+						center: pyrmont,
+						zoom: 10 
+					 });
+					 
+					 var request = {
+						location: pyrmont,
+						radius: 2000,
+						types: ['restaurant']
+					  };
+
+					  placesList = document.getElementById('places');
+
+					  var service = new google.maps.places.PlacesService(map);
+					  service.nearbySearch(request, callback);
+				   
+
+				});
+
+
+					
+			} 
+
+			  
+		}
+		$scope.callback = function (results, status, pagination) {
+		  if (status != google.maps.places.PlacesServiceStatus.OK) {
+			return;
+		  } else {
+			createMarkers(results);
+
+			if (pagination.hasNextPage) {
+			  var moreButton = document.getElementById('more');
+
+			  moreButton.disabled = false;
+
+			  google.maps.event.addDomListenerOnce(moreButton, 'click',
+				  function() {
+				moreButton.disabled = true;
+				pagination.nextPage();
+			  });
+			}
+		  }
+		}
+		$scope.createMarkers = function (places) {
+		  var bounds = new google.maps.LatLngBounds();
+
+		  for (var i = 0, place; place = places[i]; i++) {
+			var image = {
+			  url: place.icon,
+			  size: new google.maps.Size(71, 71),
+			  origin: new google.maps.Point(0, 0),
+			  anchor: new google.maps.Point(17, 34),
+			  scaledSize: new google.maps.Size(25, 25)
+			};
+
+			var marker = new google.maps.Marker({
+			  map: $scope.map,
+			  icon: image,
+			  title: place.name,
+			  position: place.geometry.location
+			});
+
+			placesList.innerHTML += '<li>' + place.name + '</li>';
+
+			bounds.extend(place.geometry.location);
+		  }
+		  map.fitBounds(bounds);
+		}
+
+		//google.maps.event.addDomListener(window, 'load', initialize);
+  
+  });
+  
   
 })();
 
