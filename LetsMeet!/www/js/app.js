2983f2d9ca2f2fe00e21d8324c7d2d4cacc1dc23
Danny Coenen <info@dannycoenen.nl>
2015-05-29 10:08:28 +0200

    Framework Onsen
    App geupdate naar het Framework Onsen. Sommige functionaliteiten moeten
    nog compatible worden gemaakt met angularjs.

---
+++ LetsMeet!/www/js/app.js
@@ -0,0 +1,57 @@
+(function(){
+  'use strict';
+  var module = angular.module('app', ['onsen']);
+
+  module.controller('AppController', function($scope, $data) {
+    $scope.doSomething = function() {
+      alert('Hello, World!');
+    };
+	
+  });
+  
+  module.controller('InviteController', function($scope, $data) {
+	$scope.register = function() {
+		var name = $scope.name;
+		var phone = $scope.phone;
+		if (name == null || phone == "") {
+			alert("Vul een naam en telefoon nummer in");
+			return false;
+		}
+		localStorage.setItem("name", name);
+		localStorage.setItem("phone", phone);
+		myNavigator.pushPage('dashboard.html', { animation : 'slide' } );
+		return false;
+	}
+  });
+
+  module.factory('$data', function() {
+      var data = {};
+      
+      data.items = [
+          { 
+              title: 'Item 1 Title',
+              label: '4h',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          { 
+              title: 'Another Item Title',
+              label: '6h',
+              desc: 'Ut enim ad minim veniam.'
+          },
+          { 
+              title: 'Yet Another Item Title',
+              label: '1day ago',
+              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+          },
+          { 
+              title: 'Yet Another Item Title',
+              label: '1day ago',
+              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+          }
+      ]; 
+      
+      return data;
+  });
+  
+})();
+
--- LetsMeet!/www/js/index.js
+++ LetsMeet!/www/js/index.js
@@ -1,12 +1,128 @@
-function inviteForm() {
-	var name = document.forms["invitation"]["fname"].value;
-	var phone = document.forms["invitation"]["fphone"].value;
-	if (name == null || phone == "") {
-		alert("Vul een naam en telefoon nummer in");
-		return false;
-	}
-	localStorage.setItem("name", name);
-	localStorage.setItem("phone", phone);
-	window.location = "dashboard.html";
-	return false;
-}
+ons.ready(function() {
+	
+	//myNavigator.pushPage('register.html', { animation : 'slide' } );
+	
+	// ####################################################################
+	// Registration page
+	// ####################################################################
+	document.addEventListener("pageinit", function(e) {
+		if (e.target.id == "register") {
+			function inviteForm() {
+				var name = document.forms["invitation"]["fname"].value;
+				var phone = document.forms["invitation"]["fphone"].value;
+				if (name == null || phone == "") {
+					alert("Vul een naam en telefoon nummer in");
+					return false;
+				}
+				localStorage.setItem("name", name);
+				localStorage.setItem("phone", phone);
+				myNavigator.pushPage('dashboard.html', { animation : 'slide' } );
+				return false;
+			}
+		}
+	}, false);
+	
+	// ####################################################################
+	// Dashboard page
+	// ####################################################################
+	document.addEventListener("pageinit", function(e) {
+		if (e.target.id == "dashboard") {
+			//localStorage.clear();
+
+			var myName = localStorage.getItem("name");
+			var myPhone = localStorage.getItem("phone");
+			document.getElementById("myName").innerHTML = myName;
+			document.getElementById("myPhone").innerHTML = myPhone;
+			
+			// Controleer elke 5 seconden op uitnodigingen
+			setInterval(function(){
+				$.ajax({
+				type: "GET",
+				url: "http://dannycoenen.nl/app/letsmeet/check.php?name=" + myName + "&phone=" + myPhone,
+				dataType: "json",
+				success: function(data) {
+					$('#count').remove();
+					var notifications = data.length;
+					$('<div id="count"></div>').insertAfter('#notification');
+					document.getElementById("count").innerHTML = notifications;
+				},
+				error: function(data) {
+					//alert("ERROR");
+					$('#count').remove();
+				}
+				});
+			},5000);
+			
+			// Functie voor het controleren op uitnodigingen
+			function checkForInvites() {
+				var myName = localStorage.getItem("name");
+				var myPhone = localStorage.getItem("phone");
+				
+				$.ajax({
+					type: "GET",
+					url: "http://dannycoenen.nl/app/letsmeet/check.php?name=" + myName + "&phone=" + myPhone,
+					dataType: "json",
+					success: function(data) {
+						for (var i = 0; i < data.length;) {
+							alert('Je hebt een nieuwe uitnodiging van: ' + data[i].firstUser);
+							i++;
+						}
+					},
+					error: function(data) {
+						alert("Geen nieuwe meldingen");
+					}
+				});
+			}
+			
+			// Functie voor het versturen van uitnodigingen
+			function inviteForm() {
+				var myName = localStorage.getItem("name");
+				var myPhone = localStorage.getItem("phone");
+			
+				var secName = document.forms["invitation"]["fname"].value;
+				var secPhone = document.forms["invitation"]["fphone"].value;
+				
+				if (secName == null || secPhone == "") {
+					alert("Vul een naam en telefoon nummer in");
+					return false;
+				}
+				
+				$.ajax({
+					type: "GET",
+					url: "http://dannycoenen.nl/app/letsmeet/register.php?name=" + myName + "&phone=" + myPhone + "&secName=" + secName + "&secPhone=" + secPhone,
+					dataType: "json",
+					success: function(data) {
+						alert(data);
+					},
+					error: function(data) {
+						alert("ERROR");
+					}
+				});
+				
+				return false;
+			}
+			
+			// Inladen van contacten
+			/*var options = new ContactFindOptions();
+			options.filter="";
+			options.multiple=true; 
+			var fields = ["*"];
+			navigator.contacts.find(fields, onSuccess, onError, options);
+			
+			function onSuccess(contacts) {
+				for (var c = 0; c < contacts.length; c++) {
+					alert(contacts[i].phoneNumber[0].value);
+					//$("#contactList").append("<li><h1>"+contacts[i].phoneNumber[0].value+"</h1></li>");
+					//$("#contactList").listview("refresh");
+				}
+			}
+			
+			function onError(){
+				alert("Some Error Occured");
+			}*/
+		}
+	}, false);
+		
+});
+
+	
