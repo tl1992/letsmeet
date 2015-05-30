ed19b1b5eed831b72e4f85a19c2e6975225cf878
Danny Coenen <info@dannycoenen.nl>
2015-05-30 16:18:41 +0200

    Contact zoeken uitgebreid
    Nieuw scherm voor uitnodigingen, afspraken op dashboard, laad icoon op
    begin pagina

--- LetsMeet!/www/js/index.js
+++ LetsMeet!/www/js/index.js
@@ -5,7 +5,6 @@ ons.ready(function() {
 	// ####################################################################
 	document.addEventListener("pageinit", function(e) {
 		if (e.target.id == "startpage") {
-			alert('start');
 		}
 	}, false);
 	
@@ -14,7 +13,6 @@ ons.ready(function() {
 	// ####################################################################
 	document.addEventListener("pageinit", function(e) {
 		if (e.target.id == "register") {
-			
 		}
 	}, false);
 	
@@ -24,7 +22,7 @@ ons.ready(function() {
 	document.addEventListener("pageinit", function(e) {
 		if (e.target.id == "dashboard") {
 			//localStorage.clear();
-
+			
 			var myName = localStorage.getItem("name");
 			var myPhone = localStorage.getItem("phone");
 			document.getElementById("myName").innerHTML = myName;
@@ -48,34 +46,6 @@ ons.ready(function() {
 				}
 				});
 			},5000);
-						
-			// Functie voor het versturen van uitnodigingen
-			function inviteForm() {
-				var myName = localStorage.getItem("name");
-				var myPhone = localStorage.getItem("phone");
-			
-				var secName = document.forms["invitation"]["fname"].value;
-				var secPhone = document.forms["invitation"]["fphone"].value;
-				
-				if (secName == null || secPhone == "") {
-					alert("Vul een naam en telefoon nummer in");
-					return false;
-				}
-				
-				$.ajax({
-					type: "GET",
-					url: "http://dannycoenen.nl/app/letsmeet/register.php?name=" + myName + "&phone=" + myPhone + "&secName=" + secName + "&secPhone=" + secPhone,
-					dataType: "json",
-					success: function(data) {
-						alert(data);
-					},
-					error: function(data) {
-						alert("ERROR");
-					}
-				});
-				
-				return false;
-			}
 		}
 	}, false);
 	
