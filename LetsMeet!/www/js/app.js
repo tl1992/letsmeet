ae2d7288d9ba3ff9331e16a0ac1bf1a90e1a7cf8
tl1992 <info@morgenmedia.nl>
2015-05-30 13:50:47 +0200

    Locatiefunctionaliteiten geoptimaliseerd
    Locatiefunctionaliteiten geoptimaliseerd voor Angular JS

--- LetsMeet!/www/js/app.js
+++ LetsMeet!/www/js/app.js
@@ -105,7 +105,113 @@
 	}
   });
   
-  var app = angular.module("geo", ["ui.map", "ui.event"])
+  
+	//Angular App Module and Controller
+	angular.module('mapsApp', [])
+	module.controller('MapCtrl', function ($scope) {
+		
+		
+		 $scope.showPosition = function (position) {
+			 
+			var pyrmont = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			var mapOptions = {
+				zoom: 16,
+				center: pyrmont,
+				mapTypeId: google.maps.MapTypeId.TERRAIN
+			}
+
+			$scope.map = new google.maps.Map(document.getElementById('maps'), mapOptions);
+			
+			 var request = {
+				location: pyrmont,
+				radius: 2000,
+				types: ['restaurant']
+			  };
+
+			  $scope.placesList = document.getElementById('places');
+
+			  $scope.service = new google.maps.places.PlacesService(map);
+			  $scope.service.nearbySearch(request, $scope.callback);
+		 }
+		 
+		 $scope.callback = function (results, status, pagination) {
+		  if (status != google.maps.places.PlacesServiceStatus.OK) {
+			return;
+		  } else {
+			$scope.createMarkers(results);
+
+			/*if (pagination.hasNextPage) {
+			  var moreButton = document.getElementById('more');
+
+			  moreButton.disabled = false;
+
+			  google.maps.event.addDomListenerOnce(moreButton, 'click',
+				  function() {
+				moreButton.disabled = true;
+				pagination.nextPage();
+			  });
+			}*/
+		  }
+		}
+		$scope.createMarkers = function (places) {
+		  $scope.bounds = new google.maps.LatLngBounds();
+
+		  for (var i = 0, place; place = places[i]; i++) {
+			var image = {
+			  url: place.icon,
+			  size: new google.maps.Size(71, 71),
+			  origin: new google.maps.Point(0, 0),
+			  anchor: new google.maps.Point(17, 34),
+			  scaledSize: new google.maps.Size(25, 25)
+			};
+
+			var marker = new google.maps.Marker({
+			  map: $scope.map,
+			  icon: image,
+			  title: place.name,
+			  position: place.geometry.location
+			});
+
+			$scope.placesList.innerHTML += '<li>' + place.name + '</li>';
+
+			$scope.bounds.extend(place.geometry.location);
+		  }
+		  $scope.map.fitBounds($scope.bounds);
+		}
+		 
+		 $scope.showError = function (error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    $scope.error = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    $scope.error = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    $scope.error = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    $scope.error = "An unknown error occurred."
+                    break;
+            }
+            $scope.$apply();
+        }
+		
+		
+		$scope.getLocation = function () {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+            }
+            else {
+                $scope.error = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        $scope.getLocation();
+
+	});
+   
+	/*var app = angular.module("geo", ["ui.map", "ui.event"])
     module.controller("MapController", function ($scope) {
         $scope.lat = "0";
         $scope.lng = "0";
@@ -118,7 +224,9 @@
             return $scope.error == "";
         }
 
-        $scope.mapOptions = {
+       
+
+	   $scope.mapOptions = {
             center: new google.maps.LatLng($scope.lat, $scope.lng),
             zoom: 15,
             mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -130,11 +238,13 @@
             $scope.accuracy = position.coords.accuracy;
             $scope.$apply();
 
-            var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
-			alert($scope.model.myMap);
+            var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
             $scope.model.myMap.setCenter(latlng);
             $scope.myMarkers.push(new google.maps.Marker({ map: $scope.model.myMap, position: latlng }));
         }
+		
+		
 
         $scope.showError = function (error) {
             switch (error.code) {
@@ -164,7 +274,7 @@
         }
 
         $scope.getLocation();
-    });
+    });*/
   
   
   /*module.controller('MapController', function ($scope) {
