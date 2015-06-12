909ce48156b66b9a78994e0b96675329fe5abd4b
tl1992 <info@morgenmedia.nl>
2015-06-12 13:25:26 +0200

    Revert "Revert "Revert "Android en cordova toegevoegd"""
    This reverts commit 6e357fdeb992247280ac70cc7731de3d0cd2c47d.

--- LetsMeet!/www/js/index.js
+++ LetsMeet!/www/js/index.js
@@ -1,18 +1,6 @@
-ons.ready(function() {	
-	document.addEventListener("pageinit", function(e) {
-			if (e.target.id == "notification") {
-
-			alert("hoi");
-			
-			var telephoneNumber = cordova.require("cordova/plugin/telephonenumber");
-			telephoneNumber.get(function(result) {
-					alert("result = " + result);
-				}, function(error) {
-					alert("error = " + error.code);
-				});
-				
-			}
-
-			
-	});
+ons.ready(function() {
+	
+		
 });
+
+	
